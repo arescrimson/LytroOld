f2f696e4520d898c42be6b9d6c701f6fcc3a08a4
areszhang25 <areszee7@gmail.com>
2023-09-12 15:17:50 -0700

    Command Functionality
    Added basic command functionality

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
+const axios = require('axios');
 
 const { Client, IntentsBitField } = require('discord.js');
+const PREFIX = '!';
+
+const JIKAN_API_BASE_URL = 'https://api.jikan.moe/v3';
 
 const client = new Client({
     intents: [
@@ -12,13 +16,21 @@ const client = new Client({
 });
 
 client.on('ready', (c) => {
-    console.log(`${c.user.tag + " is ready."}`)
+    console.log(`${c.user.tag + " is ready."}`);
 })
 
 client.on('messageCreate', async (message) => {
 
-    if (message.author.bot) return
+    if (message.author.bot) return;
 
+    if (message.content.startsWith(PREFIX)) { 
+        const commandArgs = message.content.slice(PREFIX.length).trim().split(/ +/);
+        const command = commandArgs.shift().toLowerCase();
+
+        if (command === 'test') { 
+            message.channel.send('testing message.');
+        }
+    }
     
 })
 
