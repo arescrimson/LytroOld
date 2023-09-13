c7d29b0b56490c089d2ff24b676202291a0ccaf4
areszhang25 <areszee7@gmail.com>
2023-09-12 21:39:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const { Client, IntentsBitField } = require('discord.js');
 const PREFIX = '!';
 
-const JIKAN_API_BASE_URL = 'https://api.jikan.moe/v3';
+const JIKAN_API_BASE_URL = 'https://api.jikan.moe/v4';
 
 const client = new Client({
     intents: [
@@ -19,6 +19,8 @@ client.on('ready', (c) => {
     console.log(`${c.user.tag + " is ready."}`);
 })
 
+
+
 client.on('messageCreate', async (message) => {
 
     if (message.author.bot) return;
@@ -27,8 +29,22 @@ client.on('messageCreate', async (message) => {
         const commandArgs = message.content.slice(PREFIX.length).trim().split(/ +/);
         const command = commandArgs.shift().toLowerCase();
 
-        if (command === 'test') { 
-            message.channel.send('testing message.');
+        if (command === 't') { 
+            const mangaName = commandArgs.join(' ');
+
+            try {
+                //message.channel.send(mangaName);
+                const response = await axios.get(`${JIKAN_API_BASE_URL}/manga?q=${mangaName}`)
+                const jsonData = JSON.stringify(response.data);
+                const parsedData = JSON.parse(jsonData);
+                const testurl = parsedData.data[0].url;
+                //const jsonData = JSON.stringify(response.data);
+
+                message.channel.send(testurl)
+            } catch (error) {
+                console.error('Error fetching manga data:', error);
+                message.channel.send('An error occurred while fetching manga data.');
+            }
         }
     }
     
@@ -36,4 +52,16 @@ client.on('messageCreate', async (message) => {
 
 
 client.login(process.env.TOKEN);
+
+process.on('SIGINT', () => {
+    console.log('Received SIGINT signal. Shutting down gracefully.');
+    client.destroy(); // Close the Discord.js client connection
+    process.exit(0); // Exit the Node.js process
+});
+
+process.on('SIGTERM', () => {
+    console.log('Received SIGTERM signal. Shutting down gracefully.');
+    client.destroy(); // Close the Discord.js client connection
+    process.exit(0); // Exit the Node.js process
+});
 
