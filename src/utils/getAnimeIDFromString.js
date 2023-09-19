8aafe9f3b5eef3ee226eb6dc8f4ba7ff6c872f04
areszhang25 <areszee7@gmail.com>
2023-09-18 23:43:20 -0700

    Update getAnimeIDFromString.js
    Completely reworked search algorithim to an actually working version. Happy with this for now although returning the zeroed index might not work for a small number of cases. However, this is much better than the previous version which used a string similarity comparison library.

--- src/utils/getAnimeIDFromString.js
+++ src/utils/getAnimeIDFromString.js
@@ -2,19 +2,30 @@
 
 //IMPORTS
 
-//STRING SIMILARITY LIBRARY 
-const stringSimilarity = require('string-similarity');
 //JIKAN API LIBRARY 
 const Jikan = require('jikan4.js')
 //JIKANJS WRAPPER LIBRARY
 const client = new Jikan.Client();
 
+async function getAnimeFromList(message, nameArray) {
+    message.channel.send(`${nameArray}`);
+}
+
 /**
- * Gets Anime ID from String. Since Jikan can only access specific animes through an ID, 
- * this utilizes a string-similarity to parse the string results and gets the ID from the 
- * one that is the best result. However, this is still in testing, and is not entirely accurate.
- * Also not sure that this is entirely necessary, given that Jikan already searches the searchString, 
- * but this will be addressed in the future. 
+ * Gets Anime ID from String. I actually feel kinda big brain for this. Basically...
+ * Firstly, Math.ceil rounds to the nearest integer greater to or equal to that number, preventing 
+ * any cases where the returned array of search string are less than 4. Although, I think that 
+ * MAL search will always return more than 4 entries, but this is also there since the anime that 
+ * people will be searching for will pretty much always be in the top 4 of returned searches, as 
+ * their algorithim does the matching work(I hope). 
+ * 
+ * It then iterates through that, and if the anime name is an exact match with what the user searched for, i.e. hunter x hunter, it returns that immediately. 
+ * else, it returns the first index of the searchResults array, which is usually close to what they are looking for. 
+ * Although this step needs some further work, it solved the issue of demon slayer, and you can now search demon 
+ * slayer and it will return the correct anime despite it being listed as kimetsu no yaiba, because we let MAL 
+ * search do the work. 
+ * 
+ * Then, we do a simple null check on the returned foundID and either return the foundID, or say that the anime was not found. 
  * 
  * @param {*} message is the discord message.
  * @param {*} searchString is the searched anime name. 
@@ -25,23 +36,25 @@ async function getAnimeIDFromString(message, searchString) {
 
         const searchResults = await client.anime.search(searchString)
 
-        const bestMatch = searchResults.reduce((best, anime) => {
-            const similarity = stringSimilarity.compareTwoStrings(searchString, anime.title.default);
-            if (similarity > best.similarity) {
-                return { similarity, anime };
-            }
-            return best;
-        }, { similarity: 0, anime: null });
+        const quarterLength = Math.ceil(searchResults.length / 4);
 
-        if (bestMatch.anime) {
-            const result = {
-                animeID: bestMatch.anime.id,
-                url: bestMatch.anime.url
-            };
-            
-            return result.animeID 
-        } else {
-            message.channel.send('No match found.');
+        let foundID;
+
+        for (let i = 0; i < quarterLength; i++) {
+            const animeName = (searchResults[i].title.default).toLowerCase();
+
+            if (searchString === animeName) {
+                return searchResults[i].id;
+            } else {
+                foundID = searchResults[0].id;
+            }
+        }
+
+        if (foundID !== undefined || null) {
+            return foundID;
+        } 
+        else {
+            message.channel.send('anime not found.');
         }
 
     } catch (error) {
