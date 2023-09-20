c37421045648fe8dddb897bfffec641a8dfab347
areszhang25 <areszee7@gmail.com>
2023-09-19 21:34:22 -0700

    Updated getAnime.js
    Added embed support, added new way of displaying synopsis that are too long for the character limit.

--- src/animecommands/getAnime.js
+++ src/animecommands/getAnime.js
@@ -1,13 +1,15 @@
 // getAnime.js
 
 //IMPORTS
-
+const { EmbedBuilder } = require('discord.js');
 //IMPORT GETID 
 const { getAnimeIDFromString } = require('../utils/getAnimeIDFromString');
 //JIKAN API LIBRARY 
 const Jikan = require('jikan4.js');
 //JIKANJS WRAPPER LIBRARY
 const client = new Jikan.Client();
+
+const maxLength = 1020;
 
 /**
  * Checks if value passed is null. If null, instead returns error Message 
@@ -35,6 +37,27 @@ async function getAnimeInfo(message, animeID) {
         const stats = await client.anime.getStatistics(animeID);
         const genres = anime.genres.map(genre => genre.name).join(', ');
 
+        //INITIALIZES SPLIT FOR SYNOPSIS THAT ARE OVER 1020 CHARACTERS 
+        let split = false;
+        let synopsis = '';
+        let synopsis2 = '';
+        let synopsis3 = '';
+        
+        //SPLITS SYNOPSIS IF TOO LONG INTO 2-3 PARAGRAPHS. 
+        if (anime.synopsis.length > maxLength) {
+            const splitSynopsis = anime.synopsis.split('\n');
+            //Because some synopsis are too long, yet are only 2 paragraphs, use ternary to check. 
+            //However, this means that if a synopsis is too long yet contains say, 4 paragraphs, this will not work. 
+            synopsis = splitSynopsis[0];
+            synopsis2 = (splitSynopsis[2] !== null) ? splitSynopsis[2] : '';
+            synopsis3 = (splitSynopsis[4] !== null) ? splitSynopsis[4] : '';
+            split = true;
+        } 
+        //else, simply assign synopsis to the anime synopsis. 
+        else {
+            synopsis = anime.synopsis;
+        }
+
         //GENRES AS A STRING LIST WITH COMMAS 
         let genreText = "";
 
@@ -49,7 +72,7 @@ async function getAnimeInfo(message, animeID) {
         //RATINGS AS AN AVERAGED SCORE STRING 
         let totalScore = 0;
         let totalVotes = 0;
-        
+
         for (const obj of stats.scores) {
             totalScore += obj.score * obj.votes;
             totalVotes += obj.votes;
@@ -59,22 +82,68 @@ async function getAnimeInfo(message, animeID) {
 
         const ratings = `Average score based off ${totalVotes.toLocaleString()} votes: ${averageScore.toFixed(2) + ' / 10'}`;
 
-        //SYNOPSIS, URL, EPISODES, RANK, GENRES, RATINGS
-        const SYNOPSIS = commandNullCheck(anime.synopsis, 'Synopsis not found.');
+        //SYNOPSIS, URL, EPISODES, GENRES, RATINGS
+        const SYNOPSIS = commandNullCheck(synopsis, 'Synopsis not found.');
         const URL = commandNullCheck(anime.url, 'URL not found.');
         const EPISODES = commandNullCheck(anime.episodes, 'Episodes not found.');
-        const RANK = commandNullCheck(anime.rank, 'Rank not found.');
         const GENRES = commandNullCheck(genres, 'Genres not found.');
         const RATINGS = commandNullCheck(ratings, 'Ratings not found.');
 
+        //if synopsis has been split, use the split synopsis' as embed does not support more than 1024 characters per value. 
+        if (split) {
+            const exampleEmbed = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle(`${anime.title.default}`)
+                .setURL(`${URL}`)
+                .setAuthor({ name: 'Lytro', url: 'https://github.com/arescrimson/Lytro' })
+                .setThumbnail('https://github.com/arescrimson/Lytro/blob/master/img/profile.jpg?raw=true')
+                .addFields(
+                    { name: '\n\u200b', value: '\n\u200b' },
+                    { name: 'Synopsis: \n\u200b', value: `${SYNOPSIS}` },
+                    { name: '\n', value: `${synopsis2}` },
+                    { name: '\n', value: `${synopsis3}\n\u200b` },
+                    { name: 'Episodes:', value: `${EPISODES}`, inline: true },
+                    { name: 'Genres:', value: `${GENRES}`, inline: true },
+                )
+                .addFields({ name: 'Ratings:', value: `${RATINGS}`, inline: true })
+                .setImage(`${anime.image.webp.default}`)
+                .setTimestamp()
+                .setFooter({ text: 'Information from Lytro' });
+
+            message.channel.send({ embeds: [exampleEmbed] });
+        } 
+        // if not split, simply just use the regular synopsis that was assigned to the anime synopsis. 
+        else {
+            const exampleEmbed = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle(`${anime.title.default}`)
+                .setURL(`${URL}`)
+                .setAuthor({ name: 'Lytro', url: 'https://github.com/arescrimson/Lytro' })
+                .setThumbnail('https://github.com/arescrimson/Lytro/blob/master/img/profile.jpg?raw=true')
+                .addFields(
+                    { name: '\n\u200b', value: '\n\u200b' },
+                    { name: 'Synopsis: \n\u200b', value: `${SYNOPSIS}\n\u200b` },
+                    { name: 'Episodes:', value: `${EPISODES}`, inline: true },
+                    { name: 'Genres:', value: `${GENRES}`, inline: true },
+                )
+                .addFields({ name: 'Ratings:', value: `${RATINGS}`, inline: true })
+                .setImage(`${anime.image.webp.default}`)
+                .setTimestamp()
+                .setFooter({ text: 'Information from Lytro' });
+
+            message.channel.send({ embeds: [exampleEmbed] });
+        }
+
         //FORMATTED SENT MESSAGE 
-        message.channel.send(`**Synopsis:**\n\n${SYNOPSIS}\n\n` +
-            `**Anime Rank:**\n\n${RANK}\n\n` +
-            `**Episodes:**\n\n${EPISODES}\n\n` +
-            `**Ratings**\n\n${RATINGS}\n\n` +
-            `**Genres:**\n\n${GENRES}\n\n` +
-            `**MyAnimeList URL:**\n\n${URL}
-                            `)
+        /*
+message.channel.send(`**Synopsis:**\n\n${SYNOPSIS}\n\n` +
+    `**Anime Rank:**\n\n${RANK}\n\n` +
+    `**Episodes:**\n\n${EPISODES}\n\n` +
+    `**Ratings**\n\n${RATINGS}\n\n` +
+    `**Genres:**\n\n${GENRES}\n\n` +
+    `**MyAnimeList URL:**\n\n${URL}
+                    `)
+                    */
     } catch (error) {
         console.error('Error:', error.message);
     }
