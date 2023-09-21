f2be6d75f508652a4ede03ca010068fa362004b5
areszhang25 <areszee7@gmail.com>
2023-09-21 01:02:15 -0700

    Create config.js
    Created config.js for better usability of global variables.

---
+++ config.js
@@ -0,0 +1,15 @@
+export const THUMBNAIL = 'https://github.com/arescrimson/Lytro/blob/master/img/profile.jpg?raw=true'
+export const ICON_URL = 'https://avatarfiles.alphacoders.com/281/281168.png';
+export const MAX_VALUE_LENGTH = 1020;
+
+export const SYNOPSIS_NOT_FOUND = 'Synopsis not found.';
+export const URL_NOT_FOUND = 'URL not found.';
+export const EPISODES_NOT_FOUND = 'Episodes not found.';
+export const GENRES_NOT_FOUND = 'Genres not found.';
+export const RATINGS_NOT_FOUND = 'Ratings not found.';
+
+export const BACKGROUND_NOT_FOUND = 'Background not found.';
+export const YEAR_NOT_FOUND = 'Year not found.';
+export const TRAILER_NOT_FOUND = 'Trailer not found.';
+export const STUDIO_NOT_FOUND = 'Studios not found.';
+export const RECOMMENDATIONS_NOT_FOUND = 'Recommendations not found.';
